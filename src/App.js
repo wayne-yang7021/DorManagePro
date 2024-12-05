7f95d2528cd98a689415867698f0389fb8269ee9
陳昱睿 <chenyurui@chenyuruideMacBook-Pro.local>
2024-12-05 11:26:47 +0800

    some admin features

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Admin from './pages/Admin';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import AdminInformation from './pages/AdminInformation';
 
 function App() {
     return (
@@ -11,8 +13,10 @@ function App() {
                     <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
                         <Routes>
                             <Route path="/" element={<Home />} />
+                            <Route path="/admin" element={<Admin />} />
                             <Route path="/about" element={<About />} />
                             <Route path="/contact" element={<Contact />}/>
+                            <Route path="/admin_information" element={<AdminInformation />} />
                         </Routes>
                     </main>
                 </div>
